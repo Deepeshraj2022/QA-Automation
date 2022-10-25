88184a7ad0f615dd3b5b20770f3b62fd35efe685
Deepesh Raj <mraj37591@gmail.com>
2022-10-25 22:59:30 +0530

    java programs 25-10-2022

---
+++ techlariousjavaclass/Add.java
@@ -0,0 +1,11 @@
+class Add
+{
+
+
+public static void main(String[] args)
+{
+int a=100;
+     a=a+100;
+System.out.println(a);
+}
+}
---
+++ techlariousjavaclass/Division2.java
@@ -0,0 +1,12 @@
+class Division2
+{
+
+
+public static void main(String[] args)
+{
+
+System.out.println(10%2);
+
+}
+
+}
